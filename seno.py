19b079e17334be19251d28fb444437310219bc60
lopezfella <d.barriosl@outlook.com>
2019-10-22 18:36:06 +0000

    primer commit

---
+++ seno.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,2*np.pi,150)
+y = np.sin(x)
+
+plt.figure()
+plt.plot(x,y)
+plt.title("Gráfica función Seno")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("grafica_seno.png")
+    
+    
+    
+    
+    
+    
